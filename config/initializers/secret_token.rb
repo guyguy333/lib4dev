f26faa43bf3d6653a8c6346c14c417555144b73e
guyguy333 <iguyguy333@gmail.com>
2013-09-23 10:38:41 +0000

    Really add initial rails structure

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lib4dev::Application.config.secret_key_base = '45e2cf657e5b15029a0b03ba90d18284fa52fbcf4dff50e5fc7730b61fe7a8ff69df8bb9bd4276e6dca24fbccf4ca473873e37976224751e96b9907fa2a9abcb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Lib4dev::Application.config.session_store :cookie_store, key: '_lib4dev_session'
